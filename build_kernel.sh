fe8fc4b7da802fbf5aa0bd0b95e1567a43e2c6af
physwizz <pollockg86@gmail.com>
2023-11-25 17:05:49 +1100

    update ksu

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+make clean && make mrproper
 export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
 export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
@@ -12,3 +13,5 @@ make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y physw
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+
+
